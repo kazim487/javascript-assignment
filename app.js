1c6c79c808296c25b46594c6ee23dc8b1a8e5567
kazim487 <kazimraza13579@gmail.com>
2026-04-13 17:03:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function calculateTotal (subTotal, tax ) {
+    return (subTotal + tax);
+
+}
+ 
+var order1 = calculateTotal (50,50)
+var order2 = calculateTotal (100,100) 
+var order3 = calculateTotal (150,200)
+
+console.log(order1,order2,order3);
+  
+
+ 
+
+
+
+
+
+ 
